0969d64ba6f1e7b202791d2b47a0a0ae742530d1
Simon Betton <simonabetton@gmail.com>
2021-08-10 16:28:20 +1200

    Add the import command

--- src/commands/import.ts
+++ src/commands/import.ts
@@ -1,30 +1,35 @@
 import { Command, flags } from "@oclif/command";
+import * as path from "path";
 import Config from "conf";
-const { exportSpace } = require("contentful-cli/lib/cmds/space_cmds/export");
+const { importSpace } = require("contentful-cli/lib/cmds/space_cmds/import");
 
 export default class Import extends Command {
-  static description = "A wrapper for the contentful-cli export command";
+  static description = "A wrapper for the contentful-cli import command";
 
   static flags = {
     help: flags.help({ char: "h" }),
-    // space_id: flags.string({ char: "s", description: "space ID" }),
+    space_id: flags.string({ char: "s", description: "space ID" }),
+    file: flags.string({ char: "f", description: "import JSON file" }),
   };
 
   async run() {
-    // const { flags } = this.parse(Export);
-    // if (flags.space_id) {
-    //   const config = new Config({
-    //     configName: flags.space_id,
-    //   });
-    //   exportSpace({
-    //     context: {
-    //       managementToken: config.get("management_token"),
-    //       activeSpaceId: flags.space_id,
-    //       activeEnvironmentId: config.get("environment_id"),
-    //     },
-    //   });
-    // } else {
-    //   this.error(`You must supply the space ID`);
-    // }
+    const { flags } = this.parse(Import);
+
+    if (flags.file && flags.space_id) {
+      const config = new Config({
+        configName: flags.space_id,
+      });
+
+      importSpace({
+        contentFile: path.basename(flags.file),
+        context: {
+          managementToken: config.get("management_token"),
+          activeSpaceId: flags.space_id,
+          activeEnvironmentId: config.get("environment_id"),
+        },
+      });
+    } else {
+      this.error(`You must supply the filename & space ID`);
+    }
   }
 }
